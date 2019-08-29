df88dcbe26888a73b0e25f978b4f816a05de1c6b
Chris Johnson <cjohnson@egbc.ca>
2019-08-29 15:54:17 -0700

    Change time limit

--- AwesomeUpload/Models/UploadViewModel.cs
+++ AwesomeUpload/Models/UploadViewModel.cs
@@ -8,23 +8,18 @@ namespace AwesomeUpload.Models
 {
     public class UploadViewModel
     {
-      
-
         public string FirstName { get; set; }
         public string LastName { get; set; }
-     
         public string UserName { get; set; }
         public IEnumerable<IFormFile> files { get; set; }
         public IFormFile File { get; set; }
         public byte[] Image { get; set; }
         public string Image64 { get { return Image != null ? Convert.ToBase64String(Image) : null; } }
-
         public string FileName { get; set; }
         public string FileExtension { get; set; }
         public long FileSize { get; set; }
         public bool PerformDelete { get; set; }
         public string SuccessMessage { get; set; }
-       
     }
 
 
