301c0951164d36b09bd9d3816062bb05658577a5
Chris Johnson <cjohnson@egbc.ca>
2019-08-28 18:34:16 -0700

    First Version

---
+++ AwesomeUpload/Models/UploadViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AwesomeUpload.Models
+{
+    public class UploadViewModel
+    {
+      
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+     
+        public string UserName { get; set; }
+      
+        public IFormFile File { get; set; }
+        public byte[] Image { get; set; }
+        public string Image64 { get { return Image != null ? Convert.ToBase64String(Image) : null; } }
+
+        public string FileName { get; set; }
+        public string FileExtension { get; set; }
+        public long FileSize { get; set; }
+        public bool PerformDelete { get; set; }
+       
+    }
+
+
+
+}
---
+++ AwesomeUpload/Models/UserViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AwesomeUpload.Models
+{
+    public class UserViewModel
+    {
+            public string UserId  { get; set; }
+            public string UserName { get; set; }
+            public string Email { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+    }
+}
